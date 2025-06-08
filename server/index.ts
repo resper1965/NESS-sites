feaabb711dae0b3f1b817f5b1deacc5735c13947
Ricardo Esper <44273656+resper1965@users.noreply.github.com>
2025-06-08 19:30:15 -0300

    Install helmet and enable in Express

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,23 @@
 import express, { type Request, Response, NextFunction } from "express";
+import helmet from "helmet";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "https:", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "https:"],
+      },
+    },
+    crossOriginResourcePolicy: false,
+  }),
+);
 
 app.use((req, res, next) => {
   const start = Date.now();
